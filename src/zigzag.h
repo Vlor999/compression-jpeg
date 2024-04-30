2c03c3ee0bf748d44c9c80d2ffbe004bdf6dd98b
Willem Adnet <willem.adnet@grenoble-inp.org>
2024-04-30 09:56:13 +0200

    ajout .h

---
+++ src/zigzag.h
@@ -0,0 +1,16 @@
+
+/**
+ * @brief Fonction qui permet de parcourir une matrice en zigzag
+ * @param matrice la matrice à parcourir
+ * @return un tableau 1D de taille 64
+ * **/
+
+double* zigzag_matrice(double** matrice);
+
+/**
+ * @brief Fonction qui permet de parcourir une matrice en zigzag mais sans boucle
+ * @param matrice la matrice à parcourir
+ * @return un tableau 1D de taille 64
+ * **/
+
+double* zigzag_matrice1(double** matrice);
