fe786872e6a99acfc89f92ae688bb6368d9ea2e5
Willem Adnet <willem.adnet@grenoble-inp.org>
2024-05-02 13:10:47 +0200

    normalement on peut tout lire

--- src/recupereimage.c
+++ src/recupereimage.c
@@ -1,7 +1,86 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include "../include/recupereimage.h"
+// #include "../include/recupereimage.h"
+
+
+typedef struct Triplet_RGB {
+    uint8_t R;
+    uint8_t G;
+    uint8_t B;
+} Triplet_RGB;
+
+typedef struct imagePGM {
+    int32_t col;
+    int32_t ligne;
+    int8_t max;
+    Triplet_RGB** tab;
+} imagePGM;
+
+imagePGM* LecturePPM(const char* filename) 
+{
+    FILE* file = fopen(filename, "rb");
+
+    if (file == NULL) 
+    {
+        fprintf(stderr, "Erreur d'ouverture: %s\n", filename);
+        return NULL;
+    }
+
+    char input[3];
+    fscanf(file, "%2s\n", input); // Read input number ("P6")
+
+    if (input[0] != 'P' || (input[1] != '6' && input[1] != '3')) 
+    {
+        fprintf(stderr, "Invalid PPM file format\n");
+        fclose(file);
+        return NULL;
+    }
+
+    int32_t col, ligne, max;
+    fscanf(file, "%d %d\n%d\n", &col, &ligne, &max); // Read image dimensions and max value
+
+    // Allocate memory for imagePGM structure
+    imagePGM* img = (imagePGM*)malloc(sizeof(imagePGM));
+    if (!img) {
+        fclose(file);
+        return NULL;
+    }
+
+    // Set image dimensions and max value
+    img->col = col;
+    img->ligne = ligne;
+    img->max = max;
+
+    // Allocate memory for image data
+    img->tab = (Triplet_RGB**)malloc(ligne * sizeof(Triplet_RGB*));
+    if (!img->tab) {
+        fclose(file);
+        free(img);
+        return NULL;
+    }
+
+    for (int i = 0; i < ligne; i++) {
+        img->tab[i] = (Triplet_RGB*)malloc(col * sizeof(Triplet_RGB));
+        if (!img->tab[i]) {
+            fclose(file);
+            free(img->tab);
+            free(img);
+            return NULL;
+        }
+    }
+
+    // Read image data
+    for (int i = 0; i < ligne; i++) {
+        for (int j = 0; j < col; j++) {
+            fread(&img->tab[i][j], sizeof(Triplet_RGB), 1, file);
+        }
+    }
+
+    fclose(file);
+    return img;
+}
+
 
 imagePGM* recupereimage(char* file){
     imagePGM* image = malloc(sizeof(imagePGM));
@@ -36,14 +115,36 @@ imagePGM* recupereimage(char* file){
     return image;
 }
 
+void libere_image(imagePGM* image){
+    for (int i = 0; i < image->ligne; i++){
+        free(image->tab[i]);
+    }
+    free(image->tab);
+    free(image);
+}
 
-// int main(){
-//     imagePGM* image = recupereimage("images/our_images/alea_image1.pgm");
-//     for (int i= 0; i < image->ligne; i++){
-//         for (int j = 0; j < image->col; j ++){
-//             printf("%d\t ", image->tab[i][j]);   
-//         }
-//         printf("\n");
+// int main(int argc, char* argv[]) {
+//     if (argc != 2) 
+//     {
+//         printf("Mettre sous la forme : %s <filename>\n", argv[0]);
+//         return 1;
 //     }
+
+//     const char* filename = argv[1];
+//     imagePGM* img = LecturePPM(filename);
+//     if (!img) {
+//         printf("Failed to read PPM file: %s\n", filename);
+//         return 1;
+//     }
+
+//     for(uint8_t i = 0; i < img->ligne; i++)
+//     {
+//         for(uint8_t j = 0; j < img->col; j++)
+//         {
+//             printf("R: %d, G: %d, B: %d\n", img->tab[i][j].R, img->tab[i][j].G, img->tab[i][j].B);
+//         }
+//     }
+
+//     libere_image(img);
 //     return 0;
 // }
