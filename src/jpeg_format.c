65b86c6302cca9767fffc0cb74e5153d986c2878
eddc <edouard.da-cruz@grenoble-inp.org>
2024-05-02 18:46:19 +0200

    ajout test

--- src/jpeg_format.c
+++ src/jpeg_format.c
@@ -5,23 +5,23 @@
 
 
 void ecrire_debut(char* file){
-    FILE* fptr = fopen(file, 'wb');
+    FILE* fptr = fopen(file, "wb");
     if (fptr == NULL){
         perror("pas reussi a créer");
     }
-    int8_t* tab = { 0xff, 0xd8,0xff,0xd9,0xff, 0xe0, 0x00, 0x10, 0x4a, 0x46, 0x49, 0x46, 0x00,0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; 
+    int8_t tab[22] = { 0xff, 0xd8,0xff,0xd9,0xff, 0xe0, 0x00, 0x10, 0x4a, 0x46, 0x49, 0x46, 0x00,0x01, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 }; 
     for (int i = 0; i < 22;i++){
         fwrite(&tab[i],sizeof(int8_t),1,fptr);
     }
-    fclose(file);
+    fclose(fptr);
 }
 
 
 void ecrire_fin(char* file){
-    FILE* fptr = fopen(file,'wb');
+    FILE* fptr = fopen(file,"wb");
     int16_t fin = 0xffd9;
     fwrite(&fin, sizeof(int16_t), 1, fptr);
-    fclose(file);
+    fclose(fptr);
 }
 
 
@@ -43,7 +43,7 @@ void ecrire_qtable( char* file, uint8_t* table_Y, uint8_t* table_CbCr){
     for (int8_t i = 0; i < 64; i++){
         fwrite(&table_CbCr[i], sizeof(uint8_t), 1, fptr);
     }
-    fclose(file);
+    fclose(fptr);
 }
 
 
@@ -63,10 +63,10 @@ void ecrire_htable(char* file, uint8_t* htable_DC_Y, uint8_t* htable_AC_Y, uint8
     }
 
 
-    int8_t precision = 0x01;                //indice 1 , et type 0 car DC pour CbCr
+    int8_t precision2 = 0x01;                //indice 1 , et type 0 car DC pour CbCr
     fwrite(&marqueur, sizeof(int16_t), 1, fptr);
     fwrite(&length, sizeof(int16_t), 1, fptr);
-    fwrite(&precision, sizeof(int8_t),1,fptr);
+    fwrite(&precision2, sizeof(int8_t),1,fptr);
     for (int8_t j = 0; j < 16; j++){
         fwrite(&htable_nb_length[0][1][j], sizeof(uint8_t), 1, fptr);
     }
@@ -75,29 +75,29 @@ void ecrire_htable(char* file, uint8_t* htable_DC_Y, uint8_t* htable_AC_Y, uint8
     }
 
 
-    int8_t precision = 0x12;                //indice 2 , et type 1 car AC pour Y
+    int8_t precision3 = 0x12;                //indice 2 , et type 1 car AC pour Y
     fwrite(&marqueur, sizeof(int16_t), 1, fptr);
     fwrite(&length, sizeof(int16_t), 1, fptr);
-    fwrite(&precision, sizeof(int8_t),1,fptr);
-    for (int8_t j = 0; j < 16; j++){
+    fwrite(&precision3, sizeof(int8_t),1,fptr);
+    for (int j = 0; j < 16; j++){
         fwrite(&htable_nb_length[1][0][j], sizeof(uint8_t), 1, fptr);
     }
-    for (int8_t i = 0; i < 162; i++){
+    for (int i = 0; i < 162; i++){
         fwrite(&htable_AC_Y[i], sizeof(uint8_t), 1, fptr);
     }
 
 
-    int8_t precision = 0x13;                //indice 3 , et type 1 car AC pour CbCr
+    int8_t precision4 = 0x13;                //indice 3 , et type 1 car AC pour CbCr
     fwrite(&marqueur, sizeof(int16_t), 1, fptr);
     fwrite(&length, sizeof(int16_t), 1, fptr);
-    fwrite(&precision, sizeof(int8_t),1,fptr);
+    fwrite(&precision4, sizeof(int8_t),1,fptr);
     for (int8_t j = 0; j < 16; j++){
         fwrite(&htable_nb_length[1][1][j], sizeof(uint8_t), 1, fptr);
     }
-    for (int8_t i = 0; i < 162; i++){
+    for (int i = 0; i < 162; i++){
         fwrite(&htable_AC_CbCr[i], sizeof(uint8_t), 1, fptr);
     }
-    fclose(file);
+    fclose(fptr);
 }   
 
 
@@ -117,7 +117,7 @@ void ecrire_SOF(char* file, uint16_t hauteur_image, uint16_t largeur_image){
     int8_t facteur_echantillonage = 0x01;
     int8_t table_quantification_Y = 0x00;               // indice de table quantification deY est 0 comme vu au dessus
     int8_t table_quantification_CbCr = 0x01;             // indice de table quantification de CbCr est 1 comme vu au dessus
-    for (int8_t i = 0; i < nb_composante; i++){         //la première composante sera pour Y et les 2 derniere pur Cb et Cr lorsque nb_composatne = 3
+    for (int i = 0; i < nb_composante; i++){         //la première composante sera pour Y et les 2 derniere pur Cb et Cr lorsque nb_composatne = 3
         fwrite(&identifiant, sizeof(int8_t),1,fptr);
         identifiant += 0x01;
         fwrite(&facteur_echantillonage, sizeof(int8_t),1,fptr);        // facteur à 4 car pour l'instant pas de ss-echantillonage
@@ -129,7 +129,7 @@ void ecrire_SOF(char* file, uint16_t hauteur_image, uint16_t largeur_image){
             fwrite(&table_quantification_Y, sizeof(int8_t),1,fptr);
         }
     }
-    fclose(file);
+    fclose(fptr);
 }
 
 void ecrire_SOS(char* file, uint8_t*** tab_MCU_huffman_Y, uint16_t nb_MCU_Y)  //,uint8_t*** tab_MCU_huffman_Cb, uint8_t*** tab_MCU_huffman_Cr )
@@ -144,7 +144,7 @@ void ecrire_SOS(char* file, uint8_t*** tab_MCU_huffman_Y, uint16_t nb_MCU_Y)  //
     int8_t identifiant = 0x00;
     int8_t indice_huffman_Y = 0x02;                     // 4 premier bits pour indice table DC et 4 bits pour table AC
     int8_t indice_huffman_CbCr = 0x13;                     // 4 premier bits pour indice table DC et 4 bits pour table AC
-    for (int8_t i = 0; i < nb_composante; i++){       //la première composante sera pour Y et les 2 derniere pur Cb et Cr lorsque nb_composatne = 3 
+    for (int i = 0; i < nb_composante; i++){       //la première composante sera pour Y et les 2 derniere pur Cb et Cr lorsque nb_composatne = 3 
         fwrite(&identifiant, sizeof(int8_t),1,fptr);
         identifiant += 0x01;
         if (i > 0){
@@ -166,11 +166,17 @@ void ecrire_SOS(char* file, uint8_t*** tab_MCU_huffman_Y, uint16_t nb_MCU_Y)  //
 
     for (uint16_t i = 0; i < nb_MCU_Y; i ++){
         for (uint8_t j = 0; j <64; j ++){
-            fwrite(&tab_MCU_huffman_Y[i][j][0], sizeof(uint8_t),1,fptr);
-            fwrite(&tab_MCU_huffman_Y[i][j][1], sizeof(uint8_t),1,fptr);
-        }
+            for (int16_t k = 1; k < tab_MCU_huffman_Y[i][j][0]; k++){
+                fwrite(&tab_MCU_huffman_Y[i][j][k], sizeof(uint8_t),1,fptr);
+                }
+            }
+    }
+    fclose(fptr);
     }
 
 
-    fclose(file);
-    }
+
+int main(){
+    ecrire_debut("test_image.jpeg");
+    return 0;
+}
