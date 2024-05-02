77c75eb7626e44fcb23fe032cbce661dc1a0e905
Willem Adnet <willem.adnet@grenoble-inp.org>
2024-05-02 13:12:09 +0200

    recup image

--- src/recupereimage.c
+++ src/recupereimage.c
@@ -1,85 +1,85 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
-// #include "../include/recupereimage.h"
+#include "../include/recupereimage.h"
 
 
-typedef struct Triplet_RGB {
-    uint8_t R;
-    uint8_t G;
-    uint8_t B;
-} Triplet_RGB;
+// typedef struct Triplet_RGB {
+//     uint8_t R;
+//     uint8_t G;
+//     uint8_t B;
+// } Triplet_RGB;
 
-typedef struct imagePGM {
-    int32_t col;
-    int32_t ligne;
-    int8_t max;
-    Triplet_RGB** tab;
-} imagePGM;
+// typedef struct imagePGM {
+//     int32_t col;
+//     int32_t ligne;
+//     int8_t max;
+//     Triplet_RGB** tab;
+// } imagePGM;
 
-imagePGM* LecturePPM(const char* filename) 
-{
-    FILE* file = fopen(filename, "rb");
+// imagePGM* LecturePPM(const char* filename) 
+// {
+//     FILE* file = fopen(filename, "rb");
 
-    if (file == NULL) 
-    {
-        fprintf(stderr, "Erreur d'ouverture: %s\n", filename);
-        return NULL;
-    }
+//     if (file == NULL) 
+//     {
+//         fprintf(stderr, "Erreur d'ouverture: %s\n", filename);
+//         return NULL;
+//     }
 
-    char input[3];
-    fscanf(file, "%2s\n", input); // Read input number ("P6")
+//     char input[3];
+//     fscanf(file, "%2s\n", input); // Read input number ("P6")
 
-    if (input[0] != 'P' || (input[1] != '6' && input[1] != '3')) 
-    {
-        fprintf(stderr, "Invalid PPM file format\n");
-        fclose(file);
-        return NULL;
-    }
+//     if (input[0] != 'P' || (input[1] != '6' && input[1] != '3')) 
+//     {
+//         fprintf(stderr, "Invalid PPM file format\n");
+//         fclose(file);
+//         return NULL;
+//     }
 
-    int32_t col, ligne, max;
-    fscanf(file, "%d %d\n%d\n", &col, &ligne, &max); // Read image dimensions and max value
+//     int32_t col, ligne, max;
+//     fscanf(file, "%d %d\n%d\n", &col, &ligne, &max); // Read image dimensions and max value
 
-    // Allocate memory for imagePGM structure
-    imagePGM* img = (imagePGM*)malloc(sizeof(imagePGM));
-    if (!img) {
-        fclose(file);
-        return NULL;
-    }
+//     // Allocate memory for imagePGM structure
+//     imagePGM* img = (imagePGM*)malloc(sizeof(imagePGM));
+//     if (!img) {
+//         fclose(file);
+//         return NULL;
+//     }
 
-    // Set image dimensions and max value
-    img->col = col;
-    img->ligne = ligne;
-    img->max = max;
+//     // Set image dimensions and max value
+//     img->col = col;
+//     img->ligne = ligne;
+//     img->max = max;
 
-    // Allocate memory for image data
-    img->tab = (Triplet_RGB**)malloc(ligne * sizeof(Triplet_RGB*));
-    if (!img->tab) {
-        fclose(file);
-        free(img);
-        return NULL;
-    }
+//     // Allocate memory for image data
+//     img->tab = (Triplet_RGB**)malloc(ligne * sizeof(Triplet_RGB*));
+//     if (!img->tab) {
+//         fclose(file);
+//         free(img);
+//         return NULL;
+//     }
 
-    for (int i = 0; i < ligne; i++) {
-        img->tab[i] = (Triplet_RGB*)malloc(col * sizeof(Triplet_RGB));
-        if (!img->tab[i]) {
-            fclose(file);
-            free(img->tab);
-            free(img);
-            return NULL;
-        }
-    }
+//     for (int i = 0; i < ligne; i++) {
+//         img->tab[i] = (Triplet_RGB*)malloc(col * sizeof(Triplet_RGB));
+//         if (!img->tab[i]) {
+//             fclose(file);
+//             free(img->tab);
+//             free(img);
+//             return NULL;
+//         }
+//     }
 
-    // Read image data
-    for (int i = 0; i < ligne; i++) {
-        for (int j = 0; j < col; j++) {
-            fread(&img->tab[i][j], sizeof(Triplet_RGB), 1, file);
-        }
-    }
+//     // Read image data
+//     for (int i = 0; i < ligne; i++) {
+//         for (int j = 0; j < col; j++) {
+//             fread(&img->tab[i][j], sizeof(Triplet_RGB), 1, file);
+//         }
+//     }
 
-    fclose(file);
-    return img;
-}
+//     fclose(file);
+//     return img;
+// }
 
 
 imagePGM* recupereimage(char* file){
