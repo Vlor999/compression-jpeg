6f658194895c4795958a9addcc3a4546728ab4f4
Edouard Da Cruz <edouard.da-cruz@grenoble-inp.org>
2024-05-21 11:28:09 +0000

    Delete MakefileTemp.sh

--- src/MakefileTemp.sh
+++
@@ -1,2 +0,0 @@
-gcc -Wall -Wextra -std=c99 -Iinclude  main.c quantification.c recupereimage.c zigzag.c decomposition_MCU.c recup_v2.c jpeg_format.c htables.c conversionRGB.c DCT.c magnetude_dc.c option_main.c -lm
-
